6da08267344a566badba1caadc6d1364a9ac1f1d
aandreev-scalefocus <43986093+aandreev-scalefocus@users.noreply.github.com>
2018-12-10 16:01:09 +0200

    Switched base image for Docker containerisation and updated build script

--- build-images.bat
+++ build-images.bat
@@ -13,3 +13,8 @@ cd ../
 cd hystrix-dashboard/
 call mvn compile com.google.cloud.tools:jib-maven-plugin:0.10.1:build -Dimage=avandreev/hystrix-dashboard
 cd ../
+call docker pull avandreev/discovery-server
+call docker pull avandreev/config-server
+call docker pull avandreev/rest-server
+call docker pull avandreev/web-client
+call docker pull avandreev/hystrix-dashboard
