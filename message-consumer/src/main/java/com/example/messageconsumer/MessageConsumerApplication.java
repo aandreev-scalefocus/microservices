5422132f751e7cce2ca0109fcc5e65307cf65ce8
aandreev-scalefocus <43986093+aandreev-scalefocus@users.noreply.github.com>
2018-12-18 10:49:26 +0200

    Update MessageConsumerApplication.java

--- message-consumer/src/main/java/com/example/messageconsumer/MessageConsumerApplication.java
+++ message-consumer/src/main/java/com/example/messageconsumer/MessageConsumerApplication.java
@@ -3,11 +3,9 @@ package com.example.messageconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@Controller
 public class MessageConsumerApplication {
 
 	public static void main(String[] args) {
