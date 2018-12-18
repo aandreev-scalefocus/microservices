b17199a6d154db6314d3281a623c918fc3e4fc01
aandreev-scalefocus <43986093+aandreev-scalefocus@users.noreply.github.com>
2018-12-18 10:33:46 +0200

    Update MessageConsumer.java

--- message-consumer/src/main/java/com/example/messageconsumer/MessageConsumer.java
+++ message-consumer/src/main/java/com/example/messageconsumer/MessageConsumer.java
@@ -8,13 +8,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageConsumer {
 
+    @SneakyThrows
     @RabbitListener(queues = "${queue.name}")
     public void receiveMessage(String message) {
-        processMessage(message);
-    }
-
-    @SneakyThrows
-    private void processMessage(String message) {
         MessageDTO messageDTO = new ObjectMapper().readValue(message, MessageDTO.class);
         System.out.println("NessageDTO: " + messageDTO.toString());
     }
