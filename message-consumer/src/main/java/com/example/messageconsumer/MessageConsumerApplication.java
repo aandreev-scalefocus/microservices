247f36e90f934d43dccde08d10eebf292a5eb5c7
aandreev-scalefocus <43986093+aandreev-scalefocus@users.noreply.github.com>
2018-12-20 12:49:32 +0200

    Changed message services to use Spring Cloud Stream instead of direct RabbitMQ implementation

--- message-consumer/src/main/java/com/example/messageconsumer/MessageConsumerApplication.java
+++ message-consumer/src/main/java/com/example/messageconsumer/MessageConsumerApplication.java
@@ -3,13 +3,22 @@ package com.example.messageconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableBinding(Sink.class)
 public class MessageConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessageConsumerApplication.class, args);
+	}
+
+	@StreamListener(Sink.INPUT)
+	public void receiveMessage(MessageDTO messageDTO){
+		System.out.println(messageDTO);
 	}
 
 }
--- message-provider/src/main/java/com/example/messageprovider/MessageProviderApplication.java
+++ message-provider/src/main/java/com/example/messageprovider/MessageProviderApplication.java
@@ -1,33 +1,42 @@
 package com.example.messageprovider;
 
-import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Random;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @Controller
-@Log4j
+@EnableBinding(Source.class)
 public class MessageProviderApplication {
 
     @Autowired
-    MessageProducer messageProducer;
+    Source source;
 
     public static void main(String[] args) {
         SpringApplication.run(MessageProviderApplication.class, args);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/send/{message}")
-    public @ResponseBody void sendMessage(@PathVariable("message") String text) {
+//    @SendTo(Source.OUTPUT)
+    public @ResponseBody MessageDTO sendMessage(@PathVariable("message") String text) {
         MessageDTO messageDTO = new MessageDTO(text, new Random().nextInt(100));
         System.out.println("Sending message: " + messageDTO);
-		messageProducer.produce(messageDTO);
+
+        source.output().send(MessageBuilder.withPayload(messageDTO).build());
+
+        return messageDTO;
     }
 }
 
